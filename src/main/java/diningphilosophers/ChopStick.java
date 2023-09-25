b14388d3015916c7087ad71e2ee9cd501fd59e74
Louna_Bashti <bashlodu59@orange.fr>
2023-09-25 15:37:35 +0200

    [UPDATE] Ajout des verrou explicites sur classe ChopStick.java

--- src/main/java/diningphilosophers/ChopStick.java
+++ src/main/java/diningphilosophers/ChopStick.java
@@ -1,7 +1,13 @@
 package diningphilosophers;
 
-public class ChopStick {
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class ChopStick {
+    private final Lock verrou = new ReentrantLock();
+    private final Condition deuxDispo = verrou.newCondition();
     private static int stickCount = 0;
     private boolean iAmFree = true;
     private final int myNumber;
@@ -10,24 +16,36 @@ public class ChopStick {
         myNumber = ++stickCount;
     }
 
-    synchronized public boolean tryTake(int delay) throws InterruptedException {
-        if (!iAmFree) {
-            wait(delay);
+    synchronized public boolean tryTake(long delay) throws InterruptedException {
+        verrou.lock();
+        try{
+            if (!iAmFree) {
+            deuxDispo.await(delay, TimeUnit.MILLISECONDS);
             if (!iAmFree) // Toujours pas libre, on abandonne
             {
                 return false; // Echec
             }
         }
-        iAmFree = false;
-        // Pas utile de faire notifyAll ici, personne n'attend qu'elle soit occupée
-        return true; // Succès
+
+            iAmFree = false;
+            // Pas utile de faire notifyAll ici, personne n'attend qu'elle soit occupée
+            return true; // Succès
+
+    } finally
+        {verrou.unlock();}
+
     }
 
     synchronized public void release() {
-        iAmFree = true;
-        notifyAll();
-        System.out.println("Stick " + myNumber + " Released");
-    }
+        verrou.lock();
+        try{
+            iAmFree = true;
+            deuxDispo.signalAll();
+            System.out.println("Stick " + myNumber + " Released");
+        }
+        finally {verrou.unlock();}
+
+        }
 
     @Override
     public String toString() {
